f8087a4495a97bfd63a7d207bbaacf207babe9a4
Vj Gajjela <vijaygajjelli@gmail.com>
2023-04-17 22:26:55 +0530

    changed order
    changed order

--- src/main/resources/schema-h2.sql
+++ src/main/resources/schema-h2.sql
@@ -1,14 +1,4 @@
 
--- Bookings
-CREATE TABLE IF NOT EXISTS bookings (
-  id BIGINT AUTO_INCREMENT PRIMARY KEY,
-  start_time TIMESTAMP NOT NULL,
-  end_time TIMESTAMP NOT NULL,
-  booked_time TIMESTAMP NOT NULL,
-  booked_by VARCHAR(30),
-  room_id BIGINT NOT NULL,
-  FOREIGN KEY (room_id) REFERENCES meeting_rooms(id)
-);
 -- Maintenance Timings
 CREATE TABLE IF NOT EXISTS maint_timings (
   id BIGINT AUTO_INCREMENT PRIMARY KEY,
@@ -22,6 +12,17 @@ CREATE TABLE IF NOT EXISTS meeting_rooms (
   capacity INT NOT NULL,
   UNIQUE (id, room_name, capacity)
 );
+-- Bookings
+CREATE TABLE IF NOT EXISTS bookings (
+  id BIGINT AUTO_INCREMENT PRIMARY KEY,
+  start_time TIMESTAMP NOT NULL,
+  end_time TIMESTAMP NOT NULL,
+  booked_time TIMESTAMP NOT NULL,
+  booked_by VARCHAR(30),
+  room_id BIGINT NOT NULL,
+  FOREIGN KEY (room_id) REFERENCES meeting_rooms(id)
+);
+
 
 DELETE FROM bookings;
 DELETE FROM maint_timings;
